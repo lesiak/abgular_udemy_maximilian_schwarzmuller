7a68c67ec575829f881f933c0a9ede0f73ca072e
lesiak <lesiak@outlook.com>
2019-05-27 16:13:27 +0200

    [WEBSHOP] Recipe list layout

--- webshop/src/app/recipes/recipe-list/recipe-list.component.ts
+++ webshop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Lasagne_-_stonesoup.jpg/1200px-Lasagne_-_stonesoup.jpg'),
+    new Recipe('A Test Recipe',
+      'This is a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Lasagne_-_stonesoup.jpg/1200px-Lasagne_-_stonesoup.jpg'),
+  ];
 
   constructor() { }
 
