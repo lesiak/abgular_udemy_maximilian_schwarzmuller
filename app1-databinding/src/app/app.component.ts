40f704155a60dc1ab94a8cbc7c9e83f95ebbdbdc
lesiak <lesiak@outlook.com>
2019-05-30 11:54:11 +0200

    [APP1] Binding to custom properties

--- app1-databinding/src/app/app.component.ts
+++ app1-databinding/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{name: 'Server1', type: 'server', content: 'Server 1 Rack 1'}];
 
 
 
