b3cd06f5d1f188cdaa82048745db9c43681f13c5
lesiak <lesiak@outlook.com>
2019-06-06 09:32:03 +0200

    [APP1] Template access in Lifecycle Hooks

--- app1-databinding/src/app/server-element/server-element.component.ts
+++ app1-databinding/src/app/server-element/server-element.component.ts
@@ -1,10 +1,14 @@
 import {
+  AfterViewInit,
   Component,
   DoCheck,
+  ElementRef,
   Input,
-  OnChanges, OnDestroy,
+  OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
+  ViewChild,
   ViewEncapsulation
 } from '@angular/core';
 
@@ -15,12 +19,14 @@ import {
   encapsulation: ViewEncapsulation.Emulated // default
 })
 export class ServerElementComponent implements
-    OnInit,
-    OnChanges,
-    DoCheck,
-    OnDestroy {
+  OnInit,
+  OnChanges,
+  AfterViewInit,
+  DoCheck,
+  OnDestroy {
 
   @Input() name: string;
+  @ViewChild('heading', {static: true}) heading: ElementRef;
 
   constructor() {
     console.log('constructor called');
@@ -28,15 +34,22 @@ export class ServerElementComponent implements
 
   ngOnInit() {
     console.log('ngOnInit called');
+    console.log('Text content: ' + this.heading.nativeElement.textContent);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('ngOnChanges called');
-    console.log(changes)
+    console.log(changes);
   }
 
   ngDoCheck(): void {
     console.log('ngDoCheck called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+    console.log('Text content: ' + this.heading.nativeElement.textContent);
+
   }
 
   ngOnDestroy(): void {
