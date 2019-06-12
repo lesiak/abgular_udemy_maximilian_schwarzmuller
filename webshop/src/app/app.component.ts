0cbb5474c385e8566c056dacc3e5dc9f6d828ae1
lesiak <lesiak@outlook.com>
2019-06-12 11:19:33 +0200

    [WEBSHOP] Navigation with event binding and ngIf

--- webshop/src/app/app.component.ts
+++ webshop/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webshop';
+
+  featureSelected = 'recipes';
+
+  onNavigate(feature: string) {
+    this.featureSelected = feature;
+  }
 }
