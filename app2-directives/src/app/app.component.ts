1c8838e13d0afeda2e94a729988e37980f52bda1
lesiak <lesiak@outlook.com>
2019-06-12 16:34:40 +0200

    [APP2] ngFor directive

--- app2-directives/src/app/app.component.ts
+++ app2-directives/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
+
+  filteredNumbers(): number[] {
+    return this.onlyOdd ? this.numbers.filter((n) => n % 2 === 1) : this.numbers;
+  }
 }
