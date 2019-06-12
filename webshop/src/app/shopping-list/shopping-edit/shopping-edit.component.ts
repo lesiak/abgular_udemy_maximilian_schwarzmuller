66d0e24016c5970b5051a20b2884460222c15946
lesiak <lesiak@outlook.com>
2019-06-12 16:01:24 +0200

    [WEBSHOP] Add ingredients to shopping list

--- webshop/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ webshop/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddClicked() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingValue: number = parseFloat(this.amountInputRef.nativeElement.value);
+    const newIngredient = new Ingredient(ingName, ingValue);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
