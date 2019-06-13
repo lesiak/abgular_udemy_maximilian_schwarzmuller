ce28c224239f1b410f775203e0a83b4c23cbe9cb
lesiak <lesiak@outlook.com>
2019-06-13 14:16:37 +0200

    [APP2] ngSwitch

--- app2-directives/src/app/app.component.ts
+++ app2-directives/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
+  value = 10;
 
   filteredNumbers(): number[] {
     return this.onlyOdd ? this.numbers.filter((n) => n % 2 === 1) : this.numbers;
