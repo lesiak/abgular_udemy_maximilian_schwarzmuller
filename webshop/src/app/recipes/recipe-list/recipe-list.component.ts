1eab3813385e8cf5b7fa2d891a57ce530b87731e
lesiak <lesiak@outlook.com>
2019-06-18 14:18:57 +0200

    [WEBSHOP] EventEmitter in RecipeService for cross-component communication

--- webshop/src/app/recipes/recipe-list/recipe-list.component.ts
+++ webshop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 
@@ -11,15 +11,11 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[];
 
-  @Output() recipeSelected = new EventEmitter<Recipe>();
-
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService) {
+  }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeSelected.emit(recipe);
-  }
 }
