58f244837d9526be84cc3ade0bd7158ba13c3765
lesiak <lesiak@outlook.com>
2019-06-18 14:36:17 +0200

    [WEBSHOP] ShoppingListService pushing data updates to component
    - used event with entire data snapshot

---
+++ webshop/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,23 @@
+import {EventEmitter, Injectable, Output} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+
+@Injectable({providedIn: 'root'})
+export class ShoppingListService {
+
+  @Output() ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.getIngredients());
+  }
+
+}
