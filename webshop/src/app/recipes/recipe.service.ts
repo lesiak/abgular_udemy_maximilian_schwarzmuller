906f0f074108d0cea4d6d46d8a5855ca08434d4d
lesiak <lesiak@outlook.com>
2019-06-19 10:19:47 +0200

    [WEBSHOP] Added ingredients to recipes

--- webshop/src/app/recipes/recipe.service.ts
+++ webshop/src/app/recipes/recipe.service.ts
@@ -1,17 +1,27 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
+    new Recipe('Lasagne',
+      'Lasagne',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Lasagne_-_stonesoup.jpg/1200px-Lasagne_-_stonesoup.jpg',
+      [
+        new Ingredient('Lasagne', 1),
+        new Ingredient('Cream', 1)
+      ]),
+    new Recipe('Pasta alla Norma',
       'This is a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Lasagne_-_stonesoup.jpg/1200px-Lasagne_-_stonesoup.jpg'),
-    new Recipe('A Test Recipe 2',
-      'This is a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Lasagne_-_stonesoup.jpg/1200px-Lasagne_-_stonesoup.jpg'),
+      'https://upload.wikimedia.org/wikipedia/commons/3/3f/Pasta_alla_Norma_%282563876877%29.jpg',
+      [
+        new Ingredient('Pasta', 1),
+        new Ingredient('Aubergine', 1),
+        new Ingredient('Tomatoe', 2)
+      ]),
   ];
 
   getRecipes(): Recipe[] {
