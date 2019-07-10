2008043f488c16e64cd8fdf19e1d3844f7f51dfe
lesiak <lesiak@outlook.com>
2019-07-10 10:07:11 +0200

    [APP4] Edit server button & queryParams handling
    AAA

--- app4-routing/src/app/servers/edit-server/edit-server.component.ts
+++ app4-routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,13 +12,20 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.queryParams);
-    console.log(this.route.snapshot.fragment);
+    // console.log(this.route.snapshot.queryParams);
+    // We can safely rely on subscribe as the callback does not rely on a state set up in the ngOnInit synchronously
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        console.log(queryParams);
+        this.allowEdit = parseInt(queryParams['allowEdit'], 10) === 1;
+      }
+    );
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
