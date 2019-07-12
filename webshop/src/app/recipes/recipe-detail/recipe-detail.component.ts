ac51e94da3f4f9eba342bd30866871dac26b7043
lesiak <lesiak@outlook.com>
2019-07-12 09:43:38 +0200

    [WEBSHOP] RecipeEditComponent routes

--- webshop/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ webshop/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,21 +11,25 @@ import {ActivatedRoute, Params} from '@angular/router';
 export class RecipeDetailComponent implements OnInit {
 
   recipe: Recipe;
-  id: number;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = parseInt(params['id'], 10);
-        this.recipe = this.recipeService.getRecipe(this.id);
+        const id = parseInt(params['id'], 10);
+        this.recipe = this.recipeService.getRecipe(id);
       }
     );
   }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 }
