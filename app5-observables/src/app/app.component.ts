f06d8afebaeb8b4b6e47a5ad44530f6548a17e9d
lesiak <lesiak@outlook.com>
2019-07-16 13:34:58 +0200

    [APP5] RxJS subjects

--- app5-observables/src/app/app.component.ts
+++ app5-observables/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from './user.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {}
+  userActivated = false;
+
+  private userActivatedSubscription: Subscription;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userActivatedSubscription = this.userService.activatedEmitter.subscribe(activated => {
+      this.userActivated = activated;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userActivatedSubscription.unsubscribe();
+  }
+
 }
