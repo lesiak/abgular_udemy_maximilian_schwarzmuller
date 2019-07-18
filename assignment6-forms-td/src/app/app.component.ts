b0632062d0269bd7daf1a2c00d66f814aee974e8
lesiak <lesiak@outlook.com>
2019-07-18 12:37:00 +0200

    [ASSIGN6] Template-driven forms assignment solution

--- assignment6-forms-td/src/app/app.component.ts
+++ assignment6-forms-td/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('f', {static: false}) myForm: NgForm;
+
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 }
