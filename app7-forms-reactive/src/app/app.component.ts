3edf18f331338d1bacdc52dc0f4f2496076e5367
lesiak <lesiak@outlook.com>
2019-07-19 13:57:56 +0200

    [APP7-FR] Custom async validator

--- app7-forms-reactive/src/app/app.component.ts
+++ app7-forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, ValidationErrors, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
       // control names are passed as strings to prevent potential name mangling during minification
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -36,12 +37,24 @@ export class AppComponent implements OnInit {
     return (this.signUpForm.get('hobbies') as FormArray).controls;
   }
 
-  forbiddenNames(control: FormControl): { [key: string]: boolean } | null {
+  forbiddenNames(control: FormControl): ValidationErrors | null {
     if (this.forbiddenUsernames.includes(control.value)) {
       return {'nameIsForbidden': true};
     } else {
       return null;
     }
+  }
+
+  forbiddenEmails(control: FormControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return new Promise<ValidationErrors>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
   }
 
 }
