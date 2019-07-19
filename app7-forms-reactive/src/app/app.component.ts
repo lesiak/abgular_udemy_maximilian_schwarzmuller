ed209e7d0ca55b1ddbb2978315daae658a0a1de3
lesiak <lesiak@outlook.com>
2019-07-19 07:52:47 +0200

    [APP7-FR] Form validation

--- app7-forms-reactive/src/app/app.component.ts
+++ app7-forms-reactive/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +13,8 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
       // control names are passed as strings to prevent potential name mangling during minification
-      'username': new FormControl(null),
-      'email': new FormControl(null),
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male')
     });
   }
@@ -22,4 +22,5 @@ export class AppComponent implements OnInit {
   onSubmit() {
     console.log(this.signUpForm);
   }
+
 }
