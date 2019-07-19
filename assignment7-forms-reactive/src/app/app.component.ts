3322c129ea8e51322b87017daf7968b145ff610a
lesiak <lesiak@outlook.com>
2019-07-19 14:41:55 +0200

    [ASSIGN7] Added form and wired with a FormGroup

--- assignment7-forms-reactive/src/app/app.component.ts
+++ assignment7-forms-reactive/src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  projectForm: FormGroup;
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      // control names are passed as strings to prevent potential name mangling during minification
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, Validators.required),
+      'status': new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
 }
