ee442b79bf7e652980e3a244c3f1bcd7eedcc763
lesiak <lesiak@outlook.com>
2019-07-19 14:03:36 +0200

    [APP7-FR] statusChanges and valueChanges

--- app7-forms-reactive/src/app/app.component.ts
+++ app7-forms-reactive/src/app/app.component.ts
@@ -22,6 +22,12 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    this.signUpForm.valueChanges.subscribe(
+      value => console.log(value)
+    );
+    this.signUpForm.statusChanges.subscribe(
+      status => console.log(status)
+    );
   }
 
   onSubmit() {
