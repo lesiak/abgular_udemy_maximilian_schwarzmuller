3c34404f62ef1e1b67f2bf96b37725b688962b93
lesiak <lesiak@outlook.com>
2019-07-25 13:04:50 +0200

    [APP9-HTTP] Added starter code

---
+++ app9-http/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+  }
+
+  onFetchPosts() {
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+}
