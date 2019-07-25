65f2f804429ffb770d846c384e83c83d05d46e0a
lesiak <lesiak@outlook.com>
2019-07-25 15:00:29 +0200

    [APP9-HTTP] Http post to Firebase

--- app9-http/src/app/app.component.ts
+++ app9-http/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,19 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    this.http
+      .post('https://angular-udemy-93cb1.firebaseio.com/posts.json', postData)
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
