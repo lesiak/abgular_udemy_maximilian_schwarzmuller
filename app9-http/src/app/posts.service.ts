520a9339f6a497ab72ef29872f433db9046135eb
lesiak <lesiak@outlook.com>
2019-07-26 08:09:38 +0200

    [APP9-HTTP] Extracted mapping function

--- app9-http/src/app/posts.service.ts
+++ app9-http/src/app/posts.service.ts
@@ -7,20 +7,34 @@ import {Post, PostData} from './post.model';
 @Injectable({providedIn: 'root'})
 export class PostsService {
 
+  private POSTS_URL = 'https://angular-udemy-93cb1.firebaseio.com/posts.json';
+
+  static responseDataToPosts(responseData: { [key: string]: PostData }): Post[] {
+    if (!responseData) {
+      return [];
+    }
+    return Object.entries(responseData).map(PostsService.entryToPost);
+  }
+
+  static entryToPost(entry: [string, PostData]): Post {
+    return {id: entry[0], ...entry[1]};
+  }
+
   constructor(private http: HttpClient) {
   }
 
   savePost(postData: PostData) {
     // Send Http request
     this.http
-      .post<{ name: string }>('https://angular-udemy-93cb1.firebaseio.com/posts.json', postData)
+      .post<{ name: string }>(this.POSTS_URL, postData)
       .subscribe(responseData => {
         console.log(responseData);
       });
   }
 
   fetchPosts(): Observable<Post[]> {
-    return this.http.get<{ [key: string]: PostData }>('https://angular-udemy-93cb1.firebaseio.com/posts.json')
-      .pipe(map(responseData => Object.entries(responseData).map(e => ({id: e[0], ...e[1]}))));
+    return this.http.get<{ [key: string]: PostData }>(this.POSTS_URL)
+      .pipe(map(PostsService.responseDataToPosts));
   }
+
 }
