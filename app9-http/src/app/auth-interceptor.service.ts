176198d878d4d866d4433bae80fd7d2ac1166b1b
lesiak <lesiak@outlook.com>
2019-07-27 17:49:07 +0200

    [APP9-HTTP] Response interceptors

--- app9-http/src/app/auth-interceptor.service.ts
+++ app9-http/src/app/auth-interceptor.service.ts
@@ -1,10 +1,18 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('In request interceptor');
     const modifiedRequest = req.clone({headers: req.headers.append('Custom-Auth', 'xyz')});
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest)
+      .pipe(tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived:');
+          console.log(event.body);
+        }
+      }));
   }
 }
