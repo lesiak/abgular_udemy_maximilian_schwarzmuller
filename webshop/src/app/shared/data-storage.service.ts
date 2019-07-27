d5c86e951e482cbab490ca9ab25ed49eb28b39e2
lesiak <lesiak@outlook.com>
2019-07-27 22:44:06 +0200

    [WEBSHOP] Storing data in Firebase

--- webshop/src/app/shared/data-storage.service.ts
+++ webshop/src/app/shared/data-storage.service.ts
@@ -1,8 +1,15 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {RecipeService} from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeData() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://angular-udemy-webshop.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => console.log(response));
   }
 }
