7e342e39c6a5490a4cd7910cd46aac5c66a13f39
lesiak <lesiak@outlook.com>
2019-07-28 22:54:14 +0200

    [WEBSHOP] Login - SignUp switch

--- webshop/src/app/auth/auth.component.ts
+++ webshop/src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  private AuthComponentModes = AuthComponentMode;
+  private mode = AuthComponentMode.Login;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSwitchMode() {
+    this.mode = (this.mode === AuthComponentMode.Login)
+      ? AuthComponentMode.SignUp
+      : AuthComponentMode.Login;
+  }
+
+}
+
+enum AuthComponentMode {
+  SignUp,
+  Login
 }
