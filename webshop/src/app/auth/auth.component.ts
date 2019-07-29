e8da36a2ff5342f9e923cb6aef25af42b4e238a4
lesiak <lesiak@outlook.com>
2019-07-29 07:53:17 +0200

    [WEBSHOP] SignUp request

--- webshop/src/app/auth/auth.component.ts
+++ webshop/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -10,7 +11,7 @@ export class AuthComponent implements OnInit {
   private AuthComponentModes = AuthComponentMode;
   private mode = AuthComponentMode.Login;
 
-  constructor() {
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit() {
@@ -23,8 +24,26 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value)
+    if (form.invalid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    if (this.mode === AuthComponentMode.SignUp) {
+      this.signUp(email, password);
+    }
     form.reset();
+  }
+
+  signUp(email: string, password: string) {
+    this.authService
+      .signUp(email, password)
+      .subscribe(respData => {
+          console.log(respData);
+        },
+        error => {
+          console.log(error);
+        });
   }
 }
 
--- webshop/src/app/auth/auth.service.ts
+++ webshop/src/app/auth/auth.service.ts
@@ -1,15 +1,31 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
   private signUpUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.apiKey}`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  signUp() {
-
+  signUp(email: string, password: string): Observable<AuthResponseData> {
+    return this.http.post<AuthResponseData>(
+      this.signUpUrl,
+      {
+        email,
+        password,
+        returnSecureToken: true
+      });
   }
 }
