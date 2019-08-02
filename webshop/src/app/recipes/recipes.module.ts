99d6541492ed4d6d11152e9f438d4fae0b523f3f
lesiak <lesiak@outlook.com>
2019-08-02 08:46:49 +0200

    [WEBSHOP] Removed component exports from recipes module
    - they re only referenced through recipes routing module

--- webshop/src/app/recipes/recipes.module.ts
+++ webshop/src/app/recipes/recipes.module.ts
@@ -27,14 +27,6 @@ import {RecipesRoutingModule} from './recipes-routing.module';
     ReactiveFormsModule,
     NgbModule,
     RecipesRoutingModule
-  ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
   ]
 })
 export class RecipesModule {
