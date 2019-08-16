407725cecc670c982e75a9e11496d83ff5d08f61
lesiak <lesiak@outlook.com>
2019-08-16 09:04:10 +0200

    [WEBSHOP] Env variables for the prod build

--- webshop/set-env.ts
+++ webshop/set-env.ts
@@ -1,9 +1,13 @@
 const fs = require('fs');
+
 // Configure Angular `environment.ts` file path
-const targetPath = './src/environments/environment.ts';
+const environmentsDir = './src/environments/';
+const debugEnvFile = 'environment.ts';
+const prodEnvFile = 'environment.prod.ts';
 // Load node modules
 const colors = require('colors');
 require('dotenv').load();
+
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
    webshopBaseUrl: '${process.env.UDEMY_WEBSHOP_API_BASE_URL}',
@@ -11,12 +15,28 @@ const envConfigFile = `export const environment = {
    production: false
 };
 `;
-console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
-console.log(colors.grey(envConfigFile));
-fs.writeFile(targetPath, envConfigFile, err => {
-  if (err) {
-    throw console.error(err);
-  } else {
-    console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
-  }
-});
+const envConfigFileProd = `export const environment = {
+   webshopBaseUrl: '${process.env.UDEMY_WEBSHOP_API_BASE_URL}',
+   apiKey: '${process.env.UDEMY_WEBSHOP_AUTH_API_KEY}',
+   production: true
+};
+`;
+
+function writeEnvironmentFile(dir: string, fileName: string, contents: string) {
+  const path = dir + fileName;
+  console.log(colors.magenta(`The file \`${path}\` will be written with the following content: \n`));
+  console.log(colors.grey(contents));
+  fs.writeFile(path, contents, err => {
+    if (err) {
+      throw console.error(err);
+    } else {
+      console.log(colors.magenta(`Angular ${fileName} file generated correctly at ${path} \n`));
+    }
+  });
+}
+
+if (!fs.existsSync(environmentsDir)) {
+  fs.mkdirSync(environmentsDir);
+}
+writeEnvironmentFile(environmentsDir, debugEnvFile, envConfigFile);
+writeEnvironmentFile(environmentsDir, prodEnvFile, envConfigFileProd);
