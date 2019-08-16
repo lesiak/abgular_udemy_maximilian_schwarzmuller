ca2746380037940cdb05e880a57855d23f08da06
lesiak <lesiak@outlook.com>
2019-08-16 12:09:52 +0200

    [WEBSHOP] Selecting state from NgRx Store

--- webshop/src/app/shopping-list/shopping-list.component.ts
+++ webshop/src/app/shopping-list/shopping-list.component.ts
@@ -1,39 +1,32 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
-import {Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
 import {LoggingService} from '../logging.service';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit, OnDestroy {
+export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[];
+  ingredients: Observable<{ingredients: Ingredient[]}>;
 
-  private idChangedSubscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService,
-              private loggingService: LoggingService) { }
+              private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>,
+              private loggingService: LoggingService) {
+  }
 
   ngOnInit() {
-    this.ingredients = this.shoppingListService.getIngredients();
-    this.idChangedSubscription = this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
+    this.ingredients = this.store.select('shoppingList');
     this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
   }
 
   onEditItem(index: number) {
     this.shoppingListService.startedEditing.next(index);
-  }
-
-  ngOnDestroy(): void {
-    this.idChangedSubscription.unsubscribe();
   }
 
 }
