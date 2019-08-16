9ff78d72ce82b3f1167e6f51c49508ae788a10d6
lesiak <lesiak@outlook.com>
2019-08-16 11:52:40 +0200

    [WEBSHOP] NgRx reducer for shopping list

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -8,7 +8,8 @@ import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core.module';
-
+import {StoreModule} from '@ngrx/store';
+import {shoppingListReducer} from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import {CoreModule} from './core.module';
     HttpClientModule,
     NgbModule,
     AppRoutingModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     SharedModule,
     CoreModule
   ],
