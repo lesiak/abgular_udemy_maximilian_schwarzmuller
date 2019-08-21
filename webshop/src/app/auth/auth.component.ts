3b1f199bf4b8e587425b0eff5f5b914bdc1c6aa3
lesiak <lesiak@outlook.com>
2019-08-21 08:41:12 +0200

    [WEBSHOP] Cleanup

--- webshop/src/app/auth/auth.component.ts
+++ webshop/src/app/auth/auth.component.ts
@@ -1,8 +1,6 @@
 import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService, SignInResponseData, SignUpResponseData} from './auth.service';
-import {Observable, Subscription} from 'rxjs';
-import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 import {AlertComponent} from '../shared/alert/alert.component';
 import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
 import {Store} from '@ngrx/store';
@@ -23,9 +21,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   private closeSub: Subscription;
   private storeSub: Subscription;
 
-  constructor(private authService: AuthService,
-              private router: Router,
-              private componentFactoryResolver: ComponentFactoryResolver,
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
               private store: Store<fromApp.AppState>) {
   }
 
