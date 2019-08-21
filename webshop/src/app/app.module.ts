0abaa00e1ed69a4ab8dee085c59578538646c00b
lesiak <lesiak@outlook.com>
2019-08-21 08:56:31 +0200

    [WEBSHOP] Added @ngrx/store-devtools

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -12,6 +12,8 @@ import {CoreModule} from './core.module';
 import * as fromApp from './store/app.reducer';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './auth/store/auth.effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import {AuthEffects} from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule,
     CoreModule
   ],
