acd071b4cf859fcbcf075a523d9c8a863f425965
lesiak <lesiak@outlook.com>
2019-08-26 12:49:23 +0200

    [APP10-ANIM] Wildcards in transitions

--- app10-animations/src/app/app.component.ts
+++ app10-animations/src/app/app.component.ts
@@ -14,13 +14,30 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
         backgroundColor: 'yellow',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'yellow',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunk', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunk <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
@@ -32,6 +49,12 @@ export class AppComponent {
     }
 
   onAnimate() {
-    this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    const newState = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.state = newState;
+    this.wildState = newState;
+  }
+
+  onShrink() {
+    this.wildState = 'shrunk';
   }
 }
